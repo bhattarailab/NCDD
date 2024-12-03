121d18cd6ff596f7e96e053a013b50b1b21daa30
Sandesh Pokhrel <47052428+sandeshpokhrel54@users.noreply.github.com>
2024-12-03 10:46:42 +0545

    Update demo_kvasir.sh

--- ood/demo_kvasir.sh
+++ ood/demo_kvasir.sh
@@ -1,5 +1,5 @@
-# python feat_extract.py --in-dataset Kvasir_id --id_path_train './data/kvasir_data/training' --id_path_valid './data/kvasir_data/testing/ID' --out-datasets Kvasir_ood --ood_path './data/kvasir_data/testing/OOD' --name vit --model-arch vit --weights /home/sanskar/sanjay/ncdd/model_training/checkpoint/vit_timm_kvasir_pt-4_ckpt_small16.t7
-# python run_ncdd.py --in-dataset Kvasir_id --out-datasets Kvasir_ood --name vit --model-arch vit
-python run_other_algos.py --in-dataset Kvasir_id --id_path_train './data/kvasir_data/training' --id_path_valid './data/kvasir_data/testing/ID' --out-datasets Kvasir_ood --ood_path './data/kvasir_data/testing/OOD' --name vit --model-arch vit --weights /home/sanskar/sanjay/ncdd/model_training/checkpoint/vit_timm_kvasir_pt-4_ckpt_small16.t7
+python feat_extract.py --in-dataset Kvasir_id --id_path_train './data/kvasir_data/training' --id_path_valid './data/kvasir_data/testing/ID' --out-datasets Kvasir_ood --ood_path './data/kvasir_data/testing/OOD' --name vit --model-arch vit --weights model_training/checkpoint/vit_timm_kvasir_pt-4_ckpt_small16.t7
+python run_ncdd.py --in-dataset Kvasir_id --out-datasets Kvasir_ood --name vit --model-arch vit
+python run_other_algos.py --in-dataset Kvasir_id --id_path_train './data/kvasir_data/training' --id_path_valid './data/kvasir_data/testing/ID' --out-datasets Kvasir_ood --ood_path './data/kvasir_data/testing/OOD' --name vit --model-arch vit --weights model_training/checkpoint/vit_timm_kvasir_pt-4_ckpt_small16.t7
 
-# python run_other_algos.py --in-dataset Kvasir_id --out-datasets Kvasir_ood --name vit --model-arch vit --weights /home/sanskar/sanjay/ncdd/model_training/checkpoint/vit_timm_kvasir_pt-4_ckpt_small16.t7
+# python run_other_algos.py --in-dataset Kvasir_id --out-datasets Kvasir_ood --name vit --model-arch vit --weights ./model_training/checkpoint/vit_timm_kvasir_pt-4_ckpt_small16.t7
